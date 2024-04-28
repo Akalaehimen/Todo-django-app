784626a35510f1fb7e4918be54bdda753fc00f3d
Akala Ehimen <ehimenakala45@gmail.com>
2024-04-28 22:54:00 +0100

    changes to my logout'

--- endpoints/users.py
+++ endpoints/users.py
@@ -115,14 +115,14 @@ class LoginView(APIView):
 
 
 
-class LogoutView(APIView):
-    permission_classes = (IsAuthenticated,)
+# class LogoutView(APIView):
+#     permission_classes = (IsAuthenticated,)
 
-    def post(self, request):
-        request.user.auth_token.delete()
-        return Response(
-            {"message": "Logout successful"},
-            status=status.HTTP_200_OK)
+#     def post(self, request):
+#         request.user.auth_token.delete()
+#         return Response(
+#             {"message": "Logout successful"},
+#             status=status.HTTP_200_OK)
 
 
 
