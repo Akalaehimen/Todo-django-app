0e9114be17c88b31fbfe267b9f21fcaae5b5ba1e
Akala Ehimen <ehimenakala45@gmail.com>
2024-04-28 22:57:12 +0100

    changes to some db'

--- endpoints/users.py
+++ endpoints/users.py
@@ -44,73 +44,73 @@ from todos.utils import validate_email, validate_password
 #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
-class RegisterView(APIView):
-    permission_classes = (AllowAny,)
-
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            # Extract registration data from serializer
-            email = serializer.validated_data['email']
-            username = serializer.validated_data['username']
-            first_name = serializer.validated_data['first_name']
-            last_name = serializer.validated_data['last_name']
-            password = serializer.validated_data['password']  
-            
-            # check if the user is providing a valid email
-            if not validate_email(serializer.validated_data['email']):
-                return Response({"message": "Invalid email"}, status=status.HTTP_400_BAD_REQUEST)
-            
-
-            # Check if email already exists
-            if get_user_model().objects.filter(email=email).exists():
-                return Response({"message": "Email already exists"}, status=status.HTTP_400_BAD_REQUEST)
-            
-             # Check if username already exists
-            if get_user_model().objects.filter(username=username).exists():
-                return Response({"message": "Username is already Taken"}, status=status.HTTP_400_BAD_REQUEST)
-            
-            # check if the user meets the criteria for the password
-            if not validate_password(serializer.validated_data['password']):
-                return Response({"message": "   Ensure your password contains uppercase, lowercase, special character and is up to 6"}, status=status.HTTP_400_BAD_REQUEST)
-
-            # Create user with provided data
-            user = get_user_model().objects.create_user(
-                email=email,
-                username=username,
-                first_name=first_name,
-                last_name=last_name,
-                password=password
-            )
-            
-            # Additional logic as needed
-            
-            return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class LoginView(APIView):
+# class RegisterView(APIView):
 #     permission_classes = (AllowAny,)
 
 #     def post(self, request):
-#         # Extract login data from request
-#         username = request.data.get('username')
-#         password = request.data.get('password')
+#         serializer = UserSerializer(data=request.data)
+#         if serializer.is_valid():
+#             # Extract registration data from serializer
+#             email = serializer.validated_data['email']
+#             username = serializer.validated_data['username']
+#             first_name = serializer.validated_data['first_name']
+#             last_name = serializer.validated_data['last_name']
+#             password = serializer.validated_data['password']  
+            
+#             # check if the user is providing a valid email
+#             if not validate_email(serializer.validated_data['email']):
+#                 return Response({"message": "Invalid email"}, status=status.HTTP_400_BAD_REQUEST)
+            
 
-#         # Authenticate user
-#         user = authenticate(request, username=username, password=password)
+#             # Check if email already exists
+#             if get_user_model().objects.filter(email=email).exists():
+#                 return Response({"message": "Email already exists"}, status=status.HTTP_400_BAD_REQUEST)
+            
+#              # Check if username already exists
+#             if get_user_model().objects.filter(username=username).exists():
+#                 return Response({"message": "Username is already Taken"}, status=status.HTTP_400_BAD_REQUEST)
+            
+#             # check if the user meets the criteria for the password
+#             if not validate_password(serializer.validated_data['password']):
+#                 return Response({"message": "   Ensure your password contains uppercase, lowercase, special character and is up to 6"}, status=status.HTTP_400_BAD_REQUEST)
+
+#             # Create user with provided data
+#             user = get_user_model().objects.create_user(
+#                 email=email,
+#                 username=username,
+#                 first_name=first_name,
+#                 last_name=last_name,
+#                 password=password
+#             )
+            
+#             # Additional logic as needed
+            
+#             return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        # Extract login data from request
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        # Authenticate user
+        user = authenticate(request, username=username, password=password)
         
-#         if user is not None:
-#             # Login user
-#             login(request, user)
+        if user is not None:
+            # Login user
+            login(request, user)
             
-#             # Generate or retrieve token
-#             token, _ = Token.objects.get_or_create(user=user)
+            # Generate or retrieve token
+            token, _ = Token.objects.get_or_create(user=user)
             
-#             # Return token and user data
-#             return Response({'token': token.key, 'user': UserSerializer(user).data}, status=status.HTTP_200_OK)
-#         else:
-#             return Response({"message": "Invalid username or password"}, status=status.HTTP_401_UNAUTHORIZED)
+            # Return token and user data
+            return Response({'token': token.key, 'user': UserSerializer(user).data}, status=status.HTTP_200_OK)
+        else:
+            return Response({"message": "Invalid username or password"}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 
