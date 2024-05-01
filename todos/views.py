32bc04293873cc8731ec16c0fa07cd5554a62d68
Akala Ehimen <ehimenakala45@gmail.com>
2024-05-01 22:43:12 +0100

    updates on my view

--- todos/views.py
+++ todos/views.py
@@ -26,18 +26,18 @@ class Updatingtodos(APIView):
             raise Http404("Todo not foukkojnd")
 
 
-    def get(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = ToDoSerializer(snippet)
-        return Response(serializer.data)
+    # def get(self, request, pk, format=None):
+    #     snippet = self.get_object(pk)
+    #     serializer = ToDoSerializer(snippet)
+    #     return Response(serializer.data)
 
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = ToDoSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def put(self, request, pk, format=None):
+    #     snippet = self.get_object(pk)
+    #     serializer = ToDoSerializer(snippet, data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
         snippet = self.get_object(pk)
