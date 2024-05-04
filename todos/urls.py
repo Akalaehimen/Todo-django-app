bdcc81f01882416d0175b985b5a1a8440f2e348e
Akala Ehimen <ehimenakala45@gmail.com>
2024-05-04 20:52:35 +0100

    changes to url file

--- todos/urls.py
+++ todos/urls.py
@@ -3,17 +3,17 @@ from .views import *
 from endpoints.users import *
 from . import views
 
-# urlpatterns = [
-#     # path('create', views.registertodo),
-#     path('app/<int:pk>', views.Updatingtodos.as_view()),
-#     path('app', views.Creatingandgetting.as_view()),
-#     path('create', RegisterView.as_view()),
-#     path('login', LoginView.as_view()),
-#     path('logout', LogoutView.as_view()),
-#     path('delete/<int:user_id>/', DeleteUserView.as_view())
-#     # path('retrive/<int:pk>', views.retrive)
-#     # path('<int:pk>/', DetailTodo.as_view()),
-#     # path('', ListTodo.as_view()),
-#     # path('create', CreateTodo.as_view())
-#     # path('delete/<int:pk>', DeleteTodo.as_view())
-# ]
+urlpatterns = [
+    # path('create', views.registertodo),
+    path('app/<int:pk>', views.Updatingtodos.as_view()),
+    path('app', views.Creatingandgetting.as_view()),
+    path('create', RegisterView.as_view()),
+    path('login', LoginView.as_view()),
+    path('logout', LogoutView.as_view()),
+    path('delete/<int:user_id>/', DeleteUserView.as_view())
+    # path('retrive/<int:pk>', views.retrive)
+    # path('<int:pk>/', DetailTodo.as_view()),
+    # path('', ListTodo.as_view()),
+    # path('create', CreateTodo.as_view())
+    # path('delete/<int:pk>', DeleteTodo.as_view())
+]
